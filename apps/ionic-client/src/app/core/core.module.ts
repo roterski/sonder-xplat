5b9878b801770464abbdde81049692fdad404857
Piotr Roterski <pt.roterski@gmail.com>
2018-11-12 20:17:12 +0100

    CLI: ng g app.ionic client

---
+++ apps/ionic-client/src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+// libs
+import { SonderIonicCoreModule } from '@sonder/ionic';
+
+@NgModule({
+  imports: [SonderIonicCoreModule]
+})
+export class CoreModule {}
