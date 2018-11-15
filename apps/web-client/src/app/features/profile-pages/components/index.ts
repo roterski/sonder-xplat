21919dfe26c184058532ed58756600903cd25c68
Piotr Roterski <pt.roterski@gmail.com>
2018-11-15 19:08:15 +0100

    Remove unused profile-pages feature

--- apps/web-client/src/app/features/profile-pages/components/index.ts
+++
@@ -1,5 +0,0 @@
-import { ProfilePagesComponent } from './profile-pages/profile-pages.component';
-
-export const PROFILEPAGES_COMPONENTS = [ProfilePagesComponent];
-
-export * from './profile-pages/profile-pages.component';
