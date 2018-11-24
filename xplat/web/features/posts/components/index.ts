14687c04f0cd8907c76ab70dc31296aabfedf924
Piotr Roterski <pt.roterski@gmail.com>
2018-11-24 22:42:26 +0100

    Move post-list-page to containers dir

--- xplat/web/features/posts/components/index.ts
+++ xplat/web/features/posts/components/index.ts
@@ -3,15 +3,13 @@ import { CommentTreeComponent } from './comment-tree/comment-tree.component';
 import { PostItemComponent } from './post-item/post-item.component';
 import { TagChipsComponent } from './tag-chips/tag-chips.component';
 import { VoteButtonsComponent } from './vote-buttons/vote-buttons.component';
-import { PostsListPageComponent } from './posts-list-page/posts-list-page.component';
 
 export const POSTS_COMPONENTS = [
   CommentItemComponent,
   CommentTreeComponent,
   PostItemComponent,
   TagChipsComponent,
-  VoteButtonsComponent,
-  PostsListPageComponent
+  VoteButtonsComponent
 ];
 
 export * from './comment-item/comment-item.component';
@@ -19,4 +17,3 @@ export * from './comment-tree/comment-tree.component';
 export * from './post-item/post-item.component';
 export * from './tag-chips/tag-chips.component';
 export * from './vote-buttons/vote-buttons.component';
-export * from './posts-list-page/posts-list-page.component';
