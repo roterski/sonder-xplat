703070cb54b5bd30b06f01f33ca5e95585551507
Piotr Roterski <pt.roterski@gmail.com>
2018-12-02 10:52:47 +0100

    Move apollo initialization from client to libs

--- apps/web-client/src/app/app.module.ts
+++ apps/web-client/src/app/app.module.ts
@@ -3,14 +3,9 @@ import { NgModule } from '@angular/core';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 
-import { HttpClientModule } from '@angular/common/http';
-import { Apollo, ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { setContext } from 'apollo-link-context';
-
 // libs
 import { environment } from '@sonder/core';
+import { StateManagementModule } from '@sonder/features/state-management';
 
 // app
 import { CoreModule } from './core/core.module';
@@ -26,8 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CoreModule,
     SharedModule,
     AppRoutingModule,
-    ApolloModule,
-    HttpLinkModule,
+    StateManagementModule,
     environment.production
       ? []
       : [AkitaNgDevtools.forRoot(), AkitaNgRouterStoreModule.forRoot()],
@@ -35,33 +29,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PostsModule,
     BrowserAnimationsModule
   ],
-  providers: [{
-    provide: APOLLO_OPTIONS,
-    useFactory(httpLink: HttpLink, apollo: Apollo) {
-      const http = httpLink.create({
-        uri: `${environment.backendUrl}/graphql`
-      });
-
-      const auth = setContext((_, { headers }) => {
-        const token = localStorage.getItem('authToken');
-        if (!token) {
-          return {};
-        } else {
-          return { headers: {
-              ...(headers || {}),
-              'Authorization': `Bearer ${token}`
-            }
-          };
-        }
-      });
-
-      return {
-        cache: new InMemoryCache(),
-        link: auth.concat(http)
-      }
-    },
-    deps: [HttpLink]
-  }],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
--- libs/features/state-management/state-management.module.ts
+++ libs/features/state-management/state-management.module.ts
@@ -1,6 +1,47 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
+
+import { HttpClientModule } from '@angular/common/http';
+import { Apollo, ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+
+import { environment } from '@sonder/core';
+
 @NgModule({
+  imports: [
+    ApolloModule,
+    HttpLinkModule,
+  ],
   schemas: [NO_ERRORS_SCHEMA],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory(httpLink: HttpLink, apollo: Apollo) {
+      const http = httpLink.create({
+        uri: `${environment.backendUrl}/graphql`
+      });
+
+      const auth = setContext((_, { headers }) => {
+        const token = localStorage.getItem('authToken');
+        if (!token) {
+          return {};
+        } else {
+          return {
+            headers: {
+              ...(headers || {}),
+              'Authorization': `Bearer ${token}`
+            }
+          };
+        }
+      });
+
+      return {
+        cache: new InMemoryCache(),
+        link: auth.concat(http)
+      }
+    },
+    deps: [HttpLink]
+  }],
 })
 export class StateManagementModule {}
