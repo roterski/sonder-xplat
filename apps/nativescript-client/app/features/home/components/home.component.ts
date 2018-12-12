356f28a4ff04bdaaafac95b737656c7125628cfb
Piotr Roterski <pt.roterski@gmail.com>
2018-12-12 22:29:21 +0100

    CLI: ng generate @nstudio/schematics:app.nativescript client --prefix=sonder --routing

---
+++ apps/nativescript-client/app/features/home/components/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { BaseComponent } from '@sonder/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'sonder-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent extends BaseComponent {}
