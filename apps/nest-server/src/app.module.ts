7f517aeaeef8e58c850a52afb19c8c9ee231ba7e
Piotr Roterski <pt.roterski@gmail.com>
2019-03-06 21:28:28 +0100

    Make posts.controller.spec pass

--- apps/nest-server/src/app.module.ts
+++ apps/nest-server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { PostsModule } from './modules/posts/posts.module';
+import { ProfilesModule } from './modules/profiles/profiles.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule, PostsModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, PostsModule, ProfilesModule],
   controllers: [AppController],
   providers: [AppService]
 })
--- apps/nest-server/src/modules/posts/controllers/posts.controller.spec.ts
+++ apps/nest-server/src/modules/posts/controllers/posts.controller.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { PostsController } from './posts.controller';
+import { PostsService } from '../services/posts.service';
+import { Post } from '../entities';
 
 describe('Posts Controller', () => {
-  let module: TestingModule;
+  let postsController: PostsController;
+  let postsService: PostsService;
+
   beforeAll(async () => {
-    module = await Test.createTestingModule({
+    const testingModule = await Test.createTestingModule({
       controllers: [PostsController],
+      providers: [PostsService, {
+        provide: getRepositoryToken(Post),
+        useValue: {}
+      }],
     }).compile();
+
+    postsService = testingModule.get<PostsService>(PostsService);
+    postsController = testingModule.get<PostsController>(PostsController);
   });
+
   it('should be defined', () => {
-    const controller: PostsController = module.get<PostsController>(PostsController);
-    expect(controller).toBeDefined();
+    expect(postsController).toBeDefined();
   });
 });
